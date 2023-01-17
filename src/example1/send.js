734f85643e5c89bd1f9879d334266843c1104a3f
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-01-17 17:00:01 -0300

    add send

---
+++ src/example1/send.js
@@ -0,0 +1,18 @@
+const amqp = require("amqplib/callback_api");
+amqp.connect("amqp://localhost:5672", (err, connection) => {
+  if (err) throw err;
+  connection.createChannel((err, channel) => {
+    if (err) throw err;
+    const queue = "hello";
+    const msg = "Hello World!";
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+    channel.sendToQueue(queue, Buffer.from(msg));
+    console.log("[x] Sent %s", msg);
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  });
+});
