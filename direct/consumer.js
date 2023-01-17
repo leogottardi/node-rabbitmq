6f13a537c91d379417311f4c0301c0ec18f38676
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-01-17 16:59:37 -0300

    remove consume

--- direct/consumer.js
+++
@@ -1,9 +0,0 @@
-const amqp = require('amqplib')
-
-amqp.connect('amqp://guest:guest@localhost:5672').then(conn => {
-  conn.createChannel().then(ch => {
-    ch.consume('queue.direct', msg => {
-      console.log(` [x] Received ${msg.content.toString()}`)
-    }, {noAck: true})
-  })
-})
